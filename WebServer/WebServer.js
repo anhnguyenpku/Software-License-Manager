5fa89667839f32724a17ea1a60a8ce2ef46b5f27
Mathias Maes <mathiasmaesdev@gmail.com>
2018-09-12 10:38:53 +0200

    ADD Comments in WebServer.js

--- WebServer/WebServer.js
+++ WebServer/WebServer.js
@@ -33,34 +33,48 @@ web.use(fileUpload());
 //CheckAuthentication
 async function CheckAuthentication(req,res,next)
 {
+    //TODO: If no seskey and login page is requested don't validate
+
+    //Check if user has a sessionkey ready
     if(!req.cookies['seskey'])
     {
+        //No session key: redirect the user to the login page
         res.redirect("/login");
         return;
     }
 
+    //get all session info of the user
     let sesinfo = new SessionInfo(req);
 
+    //Validate the session key
     authenticator.ValidateCookie(sesinfo.cookies['seskey'],sesinfo,function(valid,err)
     {
+        //If session key is valid
         if(valid)
         {
+            //if the requested path is login
             if(req.path === "/login")
             {
+                //redirect the user to the dashboard
                 res.redirect("/");
                 return;
             }
 
+            //Serve the requested page
             next();
         }
+        //If key is invalid
         else
         {
+            //if the requested path is login
             if(req.path === "/login")
             {
+                //Serve the requested page
                 next();
             }
             else
             {
+                //redirect to the dashboard
                 res.redirect("/login");
             }
         }
@@ -112,39 +126,65 @@ web.get("/software/add",async function(req,res)
 
 web.post("/software/add", async function(req,res)
 {
+    //Get All info from the submitted form
     let software = req.body.name;
     let version = req.body.version;
     let distributor = req.body.distributor;
     let file = req.files.package;
 
+    //Register Software in the FileSystem
     app.FileSystem.RegisterSofwareVersion(software,distributor,version,file,function(sid,vid)
     {
+        //Redirect the user to the Software page that they just created
         res.redirect("/software/" + sid);
     });
 });
 
 web.post("/software/:sid/version/add",function(req,res)
 {
+    //Get Software Info from the database
     app.Database.Query("SELECT * FROM `slm_software` WHERE `id`=" + SqlScape(req.params.sid),function(software,fields,err)
     {
-        if(err) app.Error("WebServer",err.message);
-        if(software.length === 0)
+        //On Mysql Error
+        if(err)
         {
+            //Log the error
+            app.Error("WebServer",err.message);
+
+            //Redirect the user to the software page
             res.redirect("/software");
             return;
         }
 
+        //If there is no software with the specified Software Id (sid)
+        if(software.length === 0)
+        {
+            //Redirect the user to the software page
+            res.redirect("/software");
+            return;
+        }
+
+        //Get First entry from the database
         let soft = software[0];
 
+        //Register Version
         app.FileSystem.RegisterSofwareVersion(soft.name,soft.distributor,req.body.label,req.files.package,function(sid,vid)
         {
+            //If both the Software Id (sid) and Verrsion Id (vid) are not null
             if(sid && vid)
             {
+                //Redirect the user to that version page
                 res.redirect("/software/" + sid + "/version/" + vid);
+            }
+            else if(!vid)
+            {
+                //redirect the user back to the specified software page
+                res.redirect("/software/" + req.params.sid);
             }
             else
             {
-                res.redirect("/software/" + req.params.sid);
+                //Redirect the user back to the All Software page
+                res.redirect("/software");
             }
         });
     });
@@ -152,20 +192,28 @@ web.post("/software/:sid/version/add",function(req,res)
 
 web.get("/software/:sid/version/:vid",function(req,res)
 {
+    //Get all Info of the specified software from the Database
     app.Database.Query("SELECT * FROM `slm_software` WHERE `id`=" + SqlScape(req.params.sid),function(software,fields,err)
     {
+        //On Mysql Error
         if(err)
         {
+            //Log the error
             app.Error("Webserver", err.message);
+
+            //Redirect the user to the All Software page
             res.redirect("/software");
             return;
         }
+        //If there is no software with the specified Software Id (sid)
         else if(software.length == 0)
         {
+            //Redirect the user to the All Software page
             res.redirect("/software");
             return;
         }
 
+        //Get all info of the specified software version from the database
         app.Database.Query("SELECT * FROM `slm_software_versions` WHERE `id`=" + SqlScape(req.params.vid),function(versions,fields,err)
         {
             res.send(builder.BuildPage("VersionItem",
@@ -184,10 +232,18 @@ web.get("/software/:sid/version/:vid",function(req,res)
 
 web.post("/software/:sid/version/:vid",function(req,res)
 {
+    //Update the software version info
     app.Database.Query("UPDATE `slm_software_versions` SET `label`=" + SqlScape(req.body.label) + 
         " WHERE `id`=" + SqlScape(req.params.vid),function(results,fields,err)
     {
-        if(err) app.Error("WebServer",err.message);
+        //On Mysql Error
+        if(err)
+        {
+            //Log the error
+            app.Error("WebServer",err.message);
+        }
+
+        //redirect back
         res.redirect("/software/" + req.params.sid + "/version/" + req.params.vid);
     });
 });
@@ -195,26 +251,41 @@ web.post("/software/:sid/version/:vid",function(req,res)
 
 web.post("/software/:id",function(req,res)
 {
+    //Update the software info
     app.Database.Query("UPDATE `slm_software` SET `name`=" + SqlScape(req.body.name) + ", `distributor`=" + SqlScape(req.body.distributor) + 
         " WHERE `id`=" + SqlScape(req.params.id),function(results,fields,err)
     {
-        if(err) app.Error("WebServer",err.message);
+        //On Mysql error
+        if(err)
+        {
+            //Log the error
+            app.Error("WebServer",err.message);
+        }
+
+        //redirect the user back
         res.redirect("/software/" + req.params.id);
     });
 });
 
 web.get("/software/:id",function(req,res)
 {
+    //Get all info of the specified software from the database
     app.Database.Query("SELECT * FROM `slm_software` WHERE `id`=" + SqlScape(req.params.id),function(results,fields,err)
     {
+        //On Mysql Error
         if(err)
         {
+            //Log the Error
             app.Error("Webserver", err.message);
+
+            //Redirect the user to the All Software Page
             res.redirect("/software");
             return;
         }
+        //If there is no software with the specified Software Id (sid)
         else if(results.length == 0)
         {
+            //Redirect the user to the All Software Page
             res.redirect("/software");
             return;
         }
@@ -230,6 +301,8 @@ web.all("/login",async function(req,res)
 {
     res.send(builder.BuildLoginPage({}));
 });
+
+//Error Pages
 
 web.all("*",function(req,res)
 {
