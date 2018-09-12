df6c823a43500cf906a7e2d31e7faed6e3549ab8
Mathias Maes <mathiasmaesdev@gmail.com>
2018-09-12 10:52:20 +0200

    ADD QuerySync

--- modules/Mysql.js
+++ modules/Mysql.js
@@ -42,7 +42,59 @@ async function Query(querystring, callback)
 
 }
 
+function QuerySync(querystring, callback)
+{
+    pool.getConnection(function(err,con)
+    {
+        if(err)
+        {
+            //Logger.Error("Mysql",err.message);
+            callback(null,null,err);
+            return;
+        }
+
+        con.query(querystring,function(err,results,fields)
+        {
+            pool.releaseConnection(con);
+
+            if(err)
+            {
+                //Logger.Error("Mysql",err.message);
+                callback(null,null,err);
+                return;
+            }
+
+            callback(results,fields,err);
+        });
+    });
+
+}
+
 async function QueryEmpty(querystring)
+{
+    pool.getConnection(function(err,con)
+    {
+        if(err)
+        {
+            Logger.Error("Mysql",err.message);
+            return;
+        }
+
+        con.query(querystring,function(err,results,fields)
+        {
+            pool.releaseConnection(con);
+
+            if(err)
+            {
+                Logger.Error("Mysql",err.message);
+                return;
+            }
+        });
+    });
+
+}
+
+function QueryEmptySync(querystring)
 {
     pool.getConnection(function(err,con)
     {
@@ -71,4 +123,4 @@ function Stop()
     pool.end();
 }
 
-module.exports = {"Query":Query,"QueryEmpty":QueryEmpty,"Start":Start,"Stop":Stop};
+module.exports = {"Query":Query,"QueryEmpty":QueryEmpty,"QuerySync":QuerySync,"QueryEmptySync":QueryEmptySync,"Start":Start,"Stop":Stop};
